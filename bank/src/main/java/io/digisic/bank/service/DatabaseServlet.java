52835c46e208a2c48bb5daf9046bf1b3c852314f
keithpuzey <keithpuzey@users.noreply.github.com>
2024-08-27 21:20:09 +0100

    Updated query to include last transaction date

--- bank/src/main/java/io/digisic/bank/service/DatabaseServlet.java
+++ bank/src/main/java/io/digisic/bank/service/DatabaseServlet.java
@@ -34,26 +34,45 @@ public class DatabaseServlet extends HttpServlet {
             // Create a statement
             Statement statement = connection.createStatement();
 
-            // Execute the query to get the last 10 entries
-            String sql = "SELECT u.id, u.username, p.first_name, p.last_name " +
+            // Execute the query to get the last 10 registered users
+            String sql = "SELECT u.id,  p.first_name, p.last_name " +
                          "FROM users u " +
                          "LEFT JOIN user_profile p ON u.profile_id = p.id " +
                          "ORDER BY u.id DESC " +
                          "LIMIT 10";
             ResultSet resultSet = statement.executeQuery(sql);
 
-            // Process the result set
+            // Process the result set for the most recent 10 users
             out.println("<html><body><h2>Most Recent 10 Registered Users</h2><table border='1'>");
-            out.println("<tr><th>ID</th><th>Username</th><th>First Name</th><th>Last Name</th></tr>");
+            out.println("<tr><th>ID</th><th>First Name</th><th>Last Name</th></tr>");
             while (resultSet.next()) {
-                out.println("<tr><td>" + resultSet.getInt("id") + "</td><td>" + resultSet.getString("username") + "</td><td>" + 
+                out.println("<tr><td>" + resultSet.getInt("id") + "</td><td>" + "</td><td>" + 
                             (resultSet.getString("first_name") != null ? resultSet.getString("first_name") : "N/A") + "</td><td>" + 
                             (resultSet.getString("last_name") != null ? resultSet.getString("last_name") : "N/A") + "</td></tr>");
             }
-            out.println("</table></body></html>");
+            out.println("</table>");
 
-            // Close the connection
+            // Execute the query to get the last opened account
+            String accountQuery = "SELECT MAX(transaction_date) AS last_opened_account FROM account_transaction";
+            ResultSet accountResultSet = statement.executeQuery(accountQuery);
+
+            // Process the result for the last opened account
+            if (accountResultSet.next()) {
+                String lastOpenedAccount = accountResultSet.getString("last_opened_account");
+                if (lastOpenedAccount != null) {
+                    out.println("<h2>Last Opened Account Date/Time: " + lastOpenedAccount + "</h2>");
+                } else {
+                    out.println("<h2>Last Opened Account Date/Time: No data available</h2>");
+                }
+            }
+
+            // Close the connections
+            accountResultSet.close();
+            resultSet.close();
+            statement.close();
             connection.close();
+
+            out.println("</body></html>");
 
         } catch (SQLException e) {
             e.printStackTrace();
